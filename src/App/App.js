53832e3688622e56d1899a1ebaf54805c53228b5
DipalModi18 <modidipal18@gmail.com>
2020-02-27 16:14:22 -0500

    Basic structure of the Project defined

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import Content from '../Content/Content';
+import Footer from '../Footer/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
