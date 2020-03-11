1819edabf867da9a0b4b901931050e0001155bc1
saloni242 <saloni24298@gmail.com>
2020-03-10 22:59:27 -0400

    Updated Header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Header.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, Form, FormControl,Button } from 'react-bootstrap';
+
+
 
 function Header() {
   return (
     <div className="Header">
-      <h1>Header Content</h1>
+      
+        <Navbar.Brand href="#home">PlanVille</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      
     </div>
   );
 }
