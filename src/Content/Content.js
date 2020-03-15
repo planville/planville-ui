be875ca4e466691079fad99dcefd77d6c67f4558
saloni242 <saloni24298@gmail.com>
2020-03-14 22:08:45 -0400

    Updated Header and Main Content of Website

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Content.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, Form, FormControl,Button } from 'react-bootstrap';
 
 function Content() {
   return (
     <div className="Content">
-      <h1>Main Page Content</h1>
+      <Form inline>
+        
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" style={{alignItems: 'center'}} />
+          <Button variant="outline-info">Search By City</Button>
+        </Form>
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,25 +3,20 @@ import './Header.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar, Form, FormControl,Button } from 'react-bootstrap';
 
-
-
 function Header() {
   return (
+    
     <div className="Header">
       
-        <Navbar.Brand href="#home">PlanVille</Navbar.Brand>
+        <Navbar.Brand style={{color: "white"}} href="#">PlanVille</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link style={{color: "white"}} href="#devices">Devices</Nav.Link>
+          <Nav.Link style={{color: "white"}} href="#plans">Plans</Nav.Link>
+          
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
+        
       
     </div>
   );
-}
-
+  }
 export default Header;
