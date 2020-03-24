f51082f8a16d61a6bd5e88f1a862cb57d9e6d49a
DipalModi18 <modidipal18@gmail.com>
2020-03-24 01:59:46 -0400

    Provider component added

---
+++ src/Providers/Providers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Providers.css';
+
+function Providers() {
+  return (
+    <div className="Providers">
+      <h1>Providers</h1>
+    </div>
+  );
+}
+
+export default Providers;
