2dbb1f30de5192a77248343aecd78c7a8f6cb893
Dipal Modi <modidipal18@gmail.com>
2020-04-10 00:06:27 -0400

    API call integrated for Providers

--- src/Providers/Providers.js
+++ src/Providers/Providers.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import './Providers.css';
 
-const colors = ["#393E41", "#E94F37", "#1C89BF", "#A1D363",
-                   "#85FFC7", "#297373", "#FF8552", "#A40E4C"];
-
-const providers_data = [
-  {'name': 'Virgin', 'color': '#393E41', 'radius': '300px'},
-  {'name': 'Fido', 'color': '#E94F37', 'radius': '240px'},
-  {'name': 'Bell', 'color': '#1C89BF', 'radius': '200px'},
-  {'name': 'Rogers', 'color': '#A1D363', 'radius': '130px'},
-]
-
 class Provider extends React.Component {
   render = () => {
     var circleStyle = {
@@ -32,19 +22,47 @@ class Provider extends React.Component {
   }
 }
 
-function Providers() {
+class Providers extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      providers_data: []
+    }
+  }
+
+  async getProviders() {
+    
+    // eslint-disable-next-line no-useless-concat
+    const url = "http://127.0.0.1:8000/avg_rating?city_name=" + "Windsor";
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+
+  async componentDidMount() {
+    let providers_data_api = await this.getProviders();
+    console.log("Final data");
+    console.log(providers_data_api);
+
+    this.setState((state) => {
+      return {providers_data: providers_data_api}
+    })
+    console.log("State changed")
+    console.log(this.state.providers_data)
+  }
+
+  render() {
     return (
       <div>
         {
-          /* {colors.map((color) => 
-            <Provider key={color} bgColor={color}/>
-          )} */
-          providers_data.map((provider_data) => 
+          this.state.providers_data.map((provider_data) => 
             <Provider key={provider_data.name} bgColor={provider_data.color} name={provider_data.name} radius={provider_data.radius}/>
           )
         }
       </div>
     )  
+  }
 }
 
 export default Providers;
